5d2596e1b2842df52978ee4bd79751184ec571f7
Jack Groothuis <groothuj@mail.gvsu.edu>
2020-12-02 16:52:43 -0500

    made Calculator package

---
+++ Calculator/src/SimpleCalculator.java
@@ -0,0 +1,24 @@
+package Calculator;
+
+public class SimpleCalculator {
+	
+	public int add(int a, int b) {
+		return a + b;
+	}
+	
+	public int sub(int a, int b) {
+		return a - b;
+	}
+	
+	public int mul(int a, int b) {
+		return a * b;
+	}
+	
+	public int div(int a, int b) {
+		return a / b;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hey there CIS350.");
+	}
+}
