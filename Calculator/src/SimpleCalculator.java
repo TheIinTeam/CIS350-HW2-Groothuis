0a5d2bdea0b5f9ea2547b4af40dc97f617633d58
Jack Groothuis <groothuj@mail.gvsu.edu>
2020-12-03 19:18:41 -0500

    added methods

--- Calculator/src/SimpleCalculator.java
+++ Calculator/src/SimpleCalculator.java
@@ -1,24 +1,30 @@
-package Calculator;
-
 public class SimpleCalculator {
-	
-	public int add(int a, int b) {
-		return a + b;
-	}
-	
-	public int sub(int a, int b) {
-		return a - b;
-	}
-	
-	public int mul(int a, int b) {
-		return a * b;
-	}
-	
-	public int div(int a, int b) {
-		return a / b;
-	}
-	
-	public static void main(String[] args) {
-		System.out.println("Hey there CIS350.");
-	}
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int mul(int a, int b) {
+        return a * b;
+    }
+
+    public int div(int a, int b) {
+        return a / b;
+    }
+
+    public int pow(int a, int b) {
+        return a ^ b;
+    }
+
+    public int sqrt(int a) {
+        return (int) Math.sqrt(a);
+    }
+
+    public static final void main(String[] args) {
+        System.out.println("Hey there CIS350.");
+    }
 }
--- Calculator/test/SimpleCalculatorTest.java
+++ Calculator/test/SimpleCalculatorTest.java
@@ -1,46 +1,44 @@
-package Calculator;
-
 import java.util.*;
+
 import static org.junit.Assert.*;
 import org.junit.*;
 
-import jdk.jfr.Timestamp;
-import jdk.nashorn.internal.AssertsEnabled;
-
 public class SimpleCalculatorTest {
-	
-	@Test
-	public void testAdd() {
-		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.add(1, 1), 2);
-	}
-	
-	@Test
-	public void testSub() {
-		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.sub(1, 1), 0);
-	}
-	
-	@Test
-	public void testMul() {
-		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.mul(3, 5), 15);
-	}
-	
-	@Test
-	public void testDiv() {
-		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.div(8, 2), 4);
-	}
 
-	@Test
-	public void testPow() {
-		assertEquals(calc.pow(2,3), 8);
-	}
+    @Test
+    public void testAdd() {
+        SimpleCalculator calc = new SimpleCalculator();
+        assertEquals(calc.add(1, 1), 2);
+    }
 
-	@Test
-	public void testSqrt() {
-		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.sqrt(49), 7);
-	}
+
+    @Test
+    public void testSub() {
+        SimpleCalculator calc = new SimpleCalculator();
+        assertEquals(calc.sub(1, 1), 0);
+    }
+
+    @Test
+    public void testMul() {
+        SimpleCalculator calc = new SimpleCalculator();
+        assertEquals(calc.mul(3, 5), 15);
+    }
+
+    @Test
+    public void testDiv() {
+        SimpleCalculator calc = new SimpleCalculator();
+        assertEquals(calc.div(8, 2), 4);
+    }
+
+    @Test
+    public void testPow() {
+        SimpleCalculator calc = new SimpleCalculator();
+        assertEquals(calc.pow(2,3), 8);
+    }
+
+    @Test
+    public void testSqrt() {
+        SimpleCalculator calc = new SimpleCalculator();
+        assertEquals(calc.sqrt(49), 7);
+    }
 }
